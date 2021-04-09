e7494aa221ed0e8c8a7cf14696f5e916c4c4deae
anubhavanand8111 <anubhavanand8111@gmail.com>
2021-04-09 22:33:09 +0530

    delete on swipe

--- app/src/main/java/com/anubhav/superheroapp/data/db/HeroIdDao.kt
+++ app/src/main/java/com/anubhav/superheroapp/data/db/HeroIdDao.kt
@@ -1,6 +1,7 @@
 package com.anubhav.superheroapp.data.db
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
@@ -12,4 +13,7 @@ interface HeroIdDao {
 
     @Query("SELECT * FROM HeroIdData")
     suspend fun getFavHeroId():List<HeroIdData>
+
+    @Delete
+    suspend fun deleteFavHero(hero:HeroIdData)
 }
--- app/src/main/java/com/anubhav/superheroapp/ui/view/FavoriteHeroActivity.kt
+++ app/src/main/java/com/anubhav/superheroapp/ui/view/FavoriteHeroActivity.kt
@@ -1,13 +1,19 @@
 package com.anubhav.superheroapp.ui.view
 
+import android.graphics.*
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.anubhav.superheroapp.R
 import com.anubhav.superheroapp.data.db.FavIdDatabase
 import com.anubhav.superheroapp.data.db.HeroIdData
 import com.anubhav.superheroapp.ui.adapter.FavoriteHeroAdapter
+import com.google.android.material.snackbar.Snackbar
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 import kotlinx.android.synthetic.main.activity_favorite_hero.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_search.*
@@ -23,6 +29,7 @@ class FavoriteHeroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite_hero)
+        deleteOnSwipe()
 
         GlobalScope.launch(Dispatchers.IO) {
             applicationContext?.let {
@@ -46,5 +53,93 @@ class FavoriteHeroActivity : AppCompatActivity() {
 
         }
 
+
+
+    }
+    fun deleteOnSwipe(){
+        val simpleItemTouchCallback=object :ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            )=false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position =viewHolder.adapterPosition
+
+                if (direction==ItemTouchHelper.LEFT){
+                    GlobalScope.launch(Dispatchers.IO) {
+                        applicationContext?.let {
+                           favHeroList?.get(position)?.let { it1 ->
+                               FavIdDatabase(it).getFavHeroDao().deleteFavHero(
+                                   it1
+                               )
+                           }
+                            withContext(Dispatchers.Main){
+                                //Toast.makeText(, "", Toast.LENGTH_SHORT).show()
+                                Snackbar.make(favLayout,"Deleted",Snackbar.LENGTH_SHORT).show()
+
+                            }
+
+                        }
+
+                    }
+                    favHeroRv.apply {
+                        adapter?.notifyDataSetChanged()
+                    }
+                }
+
+            }
+
+            override fun onChildDraw(
+                canvas: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+
+
+//                    val itemView=viewHolder.itemView
+//                    val paint=Paint()
+//                    val icon:Bitmap
+//                    if (dX<0){
+//                        icon =BitmapFactory.decodeResource(resources,R.mipmap.ic_launcher)
+//                     paint.color=   Color.parseColor("#389E4C")
+//
+//                        canvas.drawRect(
+//
+//                            itemView.right.toFloat()+dX,itemView.top.toFloat(),
+//                            itemView.right.toFloat(),itemView.bottom.toFloat(),paint
+//                        )
+//
+//                        canvas.drawBitmap(
+//                            icon,itemView.right.toFloat(),
+//                            itemView.top.toFloat()+(itemView.bottom.toFloat()-itemView.top.toFloat()-icon.height ),paint
+//                        )
+//                    }
+//                    viewHolder.itemView.translationX=dX
+
+
+                RecyclerViewSwipeDecorator.Builder(
+                    canvas,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+                    .addBackgroundColor(Color.RED)
+                    .addActionIcon(R.drawable.ic_baseline_delete_24)
+                    .create()
+                    .decorate()
+                super.onChildDraw(canvas, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+        }
+        val itemTouchHelper=ItemTouchHelper(simpleItemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(favHeroRv)
     }
 }
